fa3574a6f9f5eacbf6e81153599b7298f11a3b54
Ivan Kuraj <ivan.kuraj@gmail.com>
2015-03-14 22:20:35 -0400

    Fix BTree enumerator
    The enumerator matches CLP one (from ICSE'15 paper) conservatively -- it only enumerates tress of the given size (not all up to that size) so it enumerates less structures

--- src/test/scala/scife/enumeration/testcases/BTreeTest.scala
+++ src/test/scala/scife/enumeration/testcases/BTreeTest.scala
@@ -20,8 +20,8 @@ import scala.reflect._
 import scala.language.postfixOps
 import scala.language.existentials
 
-class BTreeTest extends FunSuite with Matchers with GeneratorDrivenPropertyChecks with
-  HasLogger with ProfileLogger {
+@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+class BTreeTest extends FunSuite with Matchers with GeneratorDrivenPropertyChecks with HasLogger with ProfileLogger {
 
   import Checks._
   import structures._
@@ -29,7 +29,7 @@ class BTreeTest extends FunSuite with Matchers with GeneratorDrivenPropertyCheck
 
   import Util._
   import Common._
-  
+
   // size, range of keys
   type Input = (Int, Range, Int)
   type Output = BTree.Tree
@@ -41,20 +41,22 @@ class BTreeTest extends FunSuite with Matchers with GeneratorDrivenPropertyCheck
     import checkerHelper._
 
     val enum = constructEnumerator(2)
+    var res = enum(1, 1 to 1, 1)
+    def clue = res mkString "\n"
 
-    withLazyClue("Elements are: " + clue) {
-      
-      var res = enum(1, 1 to 1, 1)
+    withLazyClue("Elements are:\n" + clue) {
+
+      res = enum(1, 1 to 1, 1)
       res.size shouldBe 1
-      
-//      res = enum(1, 0 to 1, 1)
-//      res.size shouldBe 2
+
+      //      res = enum(1, 0 to 1, 1)
+      //      res.size shouldBe 2
 
       res = enum(2, 1 to 2, 1)
       res.size shouldBe 1
 
-//      res = enum(2, 0 to 2, 1)
-//      res.size shouldBe 3
+      //      res = enum(2, 0 to 2, 1)
+      //      res.size shouldBe 3
 
       res = enum(3, 1 to 3, 1)
       res.size shouldBe 1
@@ -63,136 +65,167 @@ class BTreeTest extends FunSuite with Matchers with GeneratorDrivenPropertyCheck
 
       res = enum(4, 1 to 4, 1)
       res.size shouldBe 0
-//      res = enum(4, 1 to 4, 2)
-//      res.size shouldBe 4
+      res = enum(4, 1 to 4, 2)
+      res.size shouldBe 2
       
+      res = enum(5, 1 to 5, 1)
+      res.size shouldBe 0
+      res = enum(5, 1 to 5, 2)
+      res.size shouldBe 4
+      
+      res = enum(6, 1 to 6, 1)
+      res.size shouldBe 0
+      res = enum(6, 1 to 6, 2)
+      res.size shouldBe 5
+
     }
 
+  }
+  
+  test("additions") {
+    val enum = getAdditionsEnum
+    
+    var res = enum(3, 6, 5)
+    res.size shouldBe 25
+    
+    res.toList should contain allOf (
+      List(3, 3, 0),
+      List(2, 2, 2),
+      List(1, 2, 3),
+      List(0, 5, 1)
+    )
+    res.toList should not contain List(
+      List(1, 3, 1),
+      List(0, 3, 4),
+      List(6, 0, 2),
+      List(6, 0, 2),
+      List(6, 0, 0)
+    )
+
+    res = enum(4, 0, 3)
+    res.size shouldBe 1
+    
+    res = enum(2, 1, 3)
+    res foreach print
+    res.size shouldBe 2
   }
 
   def getAdditionsEnum(implicit ms: MemoizationScope = null): DependFinite[(Int, Int, Int), List[Int]] = {
     type Input = (Int, Int, Int)
-    
+
     Depend.memoizedFin(
       (self: DependFinite[Input, List[Int]], pair: Input) => {
-        val (size, amount, max) = pair 
-        
-        if (size == 0) e.Singleton(Nil)
+        val (size, amount, max) = pair
+
+        if (size == 0 && amount > 0) e.Empty
+        else if (size == 0) e.Singleton(Nil)
+        else if (size == 1 && amount > max) e.Empty
         else {
-          e.dependent.Chain.single(Enum(0 to math.max(amount, max)),
+          e.dependent.Chain.single(Enum(0 to math.min(amount, max)),
             Depend.fin({ (added: Int) =>
               self(size - 1, amount - added, max) map {
                 added :: _
               }
-          } ))  
+            }))
         }
-      }
-    )
+      })
   }
 
   def constructEnumerator(t: Int)(implicit ms: MemoizationScope = null, ct: ClassTag[Output]) = {
-    
-    val additionsEnum = getAdditionsEnum(ms)
 
-    Depend.memoizedFin(
+    def minChildSize(h: Int) = 2 * math.pow(t, h - 1).toInt - 1
+    def maxChildSize(h: Int) = math.pow(2 * t, h).toInt - 1
+
+    val additionsEnum = getAdditionsEnum(ms)
+    
+    var nonRootNodeEnum: DependFinite[(Int, Range, Int), Output] = null
+
+    val enumChildren: DependFinite[(Int, Int, Range, Int), Output] = Depend.fin({ in: (Int, Int, Range, Int) =>
+      val (nChildren: Int, size: Int, keyRange: Range, h: Int) = in
+      info(s"nChildren=$nChildren")
+
+      val minChildSizeBelow = minChildSize(h - 1)
+      val maxChildSizeBelow = maxChildSize(h - 1)
+      val nKeys = nChildren - 1
+      val restOfNodes: Int = size - nKeys - nChildren * minChildSizeBelow
+      info(s"minChildSizeBelow=$minChildSizeBelow, maxChildSizeBelow=$maxChildSizeBelow, restOfNodes=$restOfNodes")
+      //              restOfNodes shouldBe > (0)
+
+      if (restOfNodes < 0) {
+        info(s"returing empty due to restOfNodes=$restOfNodes (input=$in," +
+          s"minChildSize(h)=${minChildSize(h)})")
+        e.Empty
+      } else {
+        e.dependent.Chain.single[List[Int], Output](
+          additionsEnum(nChildren, restOfNodes, maxChildSizeBelow - minChildSizeBelow): Finite[List[Int]],
+          Depend.fin({ (addList: List[Int]) =>
+            val childSizes = addList map { _ + minChildSizeBelow }
+            info(s"childSizes=$childSizes")
+            childSizes.size shouldBe nChildren
+
+            val keys: List[Int] = (childSizes.scanLeft(keyRange.start-1) {
+              case (soFar, el) => soFar + el + 1
+            }).tail.init
+            info(s"keys=$keys")
+            keys.size shouldBe nKeys
+
+            val childRanges =
+              ((keyRange.start-1 :: keys) zip (keys :+ (keyRange.end+1))) map {
+                case (a, b) => (a + 1) to (b - 1)
+              }
+            info(s"childRanges=$childRanges, zip=${(keyRange.start-1 :: keys) zip (keyRange.tail :+ (keyRange.end+1))}")
+            childRanges.size shouldBe nChildren
+
+            childSizes.size shouldBe childRanges.size
+            val childEnums: Array[Finite[Output]] = (childSizes zip childRanges) map {
+              case (cs, cr) => nonRootNodeEnum(cs, cr, h - 1)
+            } toArray;
+            childEnums.size shouldBe nChildren
+
+            e.Product.fin(childEnums) map {
+              children =>
+                children.size shouldBe keys.size + 1
+                Tree(keys, children)
+            }
+          }): DependFinite[List[Int], Output])
+      }
+    })
+
+    nonRootNodeEnum = Depend.memoizedFin(
       (self: DependFinite[Input, Output], pair: Input) => {
         val (size: Int, keyRange: Range, h: Int) = pair
-        
-        def minChildSize = math.pow(2 * t, h - 1).toInt - 1
-        def maxChildSize = math.pow(2 * t, h).toInt - 1
 
-        if (size <= 0 || h <= 0) throw new RuntimeException
-        else if (h == 1 && size == keyRange.size && size < 2 *t) {
-          e.Singleton( Tree(keyRange.toList, Nil) ): Finite[Output]
-        }
-        else if (h > 1 && size == keyRange.size) {
+        if (h == 1 && size == keyRange.size && size < 2 * t && size >= t - 1) {
+          e.Singleton(Tree(keyRange.toList, Nil)): Finite[Output]
+        } else if (h > 1 && size == keyRange.size && size > 0) {
           val nChildrenEnum = e.Enum(t to 2 * t)
 
-          val rootLeftPairs2: Finite[Output] = e.dependent.Chain.single(nChildrenEnum,
-            Depend.fin({ (nChildren: Int) =>
-              val nKeys = nChildren - 1
-              val restOfNodes: Int = size - nKeys - nChildren * minChildSize
-//              restOfNodes shouldBe > (0)
-              
-              if (restOfNodes < 0) {
-                info(s"input=$pair, restOfNodes=$restOfNodes")
-                e.Empty
-              }
-              else
-              e.dependent.Chain.single[List[Int], Output](
-                 additionsEnum(nChildren, restOfNodes, maxChildSize - minChildSize): Finite[List[Int]],
-                 Depend.fin({ (addList: List[Int]) =>
-                   val childSizes = addList map { _ + minChildSize }
-                   childSizes.size shouldBe nChildren
-
-                   val keys: List[Int] = (childSizes.scanLeft(keyRange.start) {
-                     case (soFar, el) => soFar + el
-                   }).tail.tail;
-                   keys.size shouldBe nKeys
-                   
-                   val childRanges =
-                     ((keyRange.start :: keys) zip (keyRange.tail :+ keyRange.end)) map {
-                       case (a, b) => (a + 1) to (b - 1)
-                     }
-                   info(s"childRanges=$childRanges")
-                   childRanges.size shouldBe nChildren
-
-                   childSizes.size shouldBe childRanges.size
-                   val childEnums: Array[Finite[Output]] = (childSizes zip childRanges) map {
-                     case (cs, cr) => self(cs, cr, h-1)
-                   } toArray;
-                   childEnums.size shouldBe nChildren
-                   
-                   e.Product.fin(childEnums) map {
-                     children =>
-                       children.size shouldBe keys.size + 1
-                       Tree(keys, children)
-                   }
-                 }): DependFinite[List[Int], Output]
-              )
-          } ))
-          
-          rootLeftPairs2: Finite[Output]
+          e.dependent.Chain.single(nChildrenEnum map { x => (x, size, keyRange, h) },
+            enumChildren): Finite[Output]
         } else {
           info(s"input=$pair")
           e.Empty //throw new RuntimeException(s"input=$pair")
         }
       })
 
+    val rootNodeEnum = Depend.memoizedFin(
+      (self: DependFinite[Input, Output], pair: Input) => {
+        val (size: Int, keyRange: Range, h: Int) = pair
+
+        if (h == 1 && size == keyRange.size && size < 2 * t) {
+          e.Singleton(Tree(keyRange.toList, Nil)): Finite[Output]
+        } else if (h > 1 && size == keyRange.size && size >= 2 * t) {
+          val nChildrenEnum = e.Enum(2 to 2 * t)
+
+          e.dependent.Chain.single(nChildrenEnum map { x => (x, size, keyRange, h) },
+            enumChildren): Finite[Output]
+        } else {
+          info(s"input at root=$pair")
+          e.Empty //throw new RuntimeException(s"input=$pair")
+        }
+      })
+
+    rootNodeEnum
   }
-//
-//  val listOfNodes: Depend[(Int, Int, Int), List[Tree]] = {
-//
-//    Depend.memoized(
-//      (self: Depend[(Int, Int, Int), List[Tree]], pair: (Int, Int, Int)) => {
-//        val (size: Int, nChildren: Int, h: Int) = pair
-//        
-//        val minPerSubtree = math.pow(2 * t, h) - 1
-//
-//        if (size <= 0 || h <= 0) throw new RuntimeException
-////        else if (h == 1) {
-////          e.Singleton( Tree(List.fill(size)(1), Nil) :: Nil )
-////        }
-//        
-//          val minSize = math.pow(2 * t, h).toInt - 1
-//          val restOfNodes: Int = size - nChildren * minSize
-//          val toAddEnum: Enum[Int] = e.Enum(0 to restOfNodes)
-//          
-//          val nodes: Enum[List[Tree]] = e.dependent.Chain.single(toAddEnum,
-//            Depend({ (toAdd: Int) =>
-//              val nodesEnum = constructEnumerator()(minSize + toAdd, 1 to 1, h)
-//              val restOfListEnum = self(size - minSize - toAdd, nChildren - 1, h)
-//
-//              e.Product(nodesEnum, restOfListEnum) map { (p: (Tree, List[Tree])) =>
-//                val (root, rest) = p
-//                root :: rest
-//              }: Enum[List[Tree]]
-//          } ))
-//          
-//          nodes
-//        
-//      })
-//
-//  }
 
 }
