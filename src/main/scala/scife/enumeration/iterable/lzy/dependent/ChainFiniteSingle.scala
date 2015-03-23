739239b7533ed22de7cd2866f89b8b9f23fe9717
Ivan Kuraj <ivan.kuraj@gmail.com>
2015-03-23 02:40:24 -0400

    Lazy BST enumeration
    Added/fixed necessary combinators

--- src/main/scala/scife/enumeration/iterable/lzy/dependent/ChainFiniteSingle.scala
+++ src/main/scala/scife/enumeration/iterable/lzy/dependent/ChainFiniteSingle.scala
@@ -50,13 +50,17 @@ class ChainFiniteSingleCombine[I, O, R]
   
   val (rightStreams, leftValues): (Array[E], Array[I]) = {
     val streams =
-      for (ind <- 0 until left.size; leftVal = left(ind);
+      for (
+//          ind <- 0 until left.size; leftVal = left(ind);
+          ind <- 0 until left.size; leftVal = left.next;
           stream = right.getEnum(leftVal); if stream.size > 0)
         yield {
         _size += stream.size
           (stream, leftVal)
         }
     info(s"streams=${streams}")
+    assert(!left.hasNext)
+    left.reset
           
     val (_rightStreams, lefts) = streams.unzip
     
@@ -74,6 +78,7 @@ class ChainFiniteSingleCombine[I, O, R]
       categoryIndex < rightStreams.size - 1
   
   override def next = {
+    require(hasNext)
     info(s"[next:] categoryIndex=$categoryIndex/${rightStreams.size}, touched=${rightStreams(categoryIndex).touched}")
     if (rightStreams(categoryIndex).touched) {
       // check if going to next element switches to next inner enum
@@ -87,6 +92,85 @@ class ChainFiniteSingleCombine[I, O, R]
     info(s"invoking super.next with $ind")
 //    super.next
     rightStreams(categoryIndex).touched = false
+    info(s"leftValues(categoryIndex)=${leftValues(categoryIndex).toString}")
+    combine( leftValues(categoryIndex), rightStreams(categoryIndex).next )   
+  }
+
+  override def size = _size
+
+  override def apply(ind: Int): R =
+    // does not work well with touched -- need to decouple
+    throw new RuntimeException
+    
+  // TODO refactor this when inner enumerators are reset iter, than this can work
+  override def hasStarted = throw new RuntimeException//leftValues(categoryIndex).hasStarted
+  
+  override def reset = {
+    // can do this along the way (during next), but this is cleaner
+    for (i <- 0 to categoryIndex)
+      rightStreams(i).reset
+    categoryIndex = 0
+  }
+    
+}
+
+class ChainFiniteSingleCombineStrict[I, O, R]
+  (left: LazyEnumFinite[I], val right: LazyDependFinite[I, O], combine: (I, O) => R)
+  (implicit ct: ClassTag[I])
+  extends
+    //e.dependent.ChainFiniteSingleCombine(left, right, combine) with
+    Finite[R] with ResetIter[R] with HasLogger {
+
+  type E = right.EnumType
+  
+  var categoryIndex = 0
+  var _size = 0
+  
+  val (rightStreams, leftValues): (Array[E], Array[I]) = {
+    val streams =
+      for (
+//          ind <- 0 until left.size; leftVal = left(ind);
+          ind <- 0 until left.size; leftVal = left.next;
+          stream = right.getEnum(leftVal); if stream.size > 0)
+        yield {
+        _size += stream.size
+          (stream, leftVal)
+        }
+    info(s"streams=${streams}")
+    assert(!left.hasNext)
+    left.reset
+          
+    val (_rightStreams, lefts) = streams.unzip
+    
+    ( _rightStreams.toArray, lefts.toArray)
+  }
+  info(s"rightStreams.size=${rightStreams.size}")
+  
+  val sizeArray = rightStreams map { _.size }
+  info(s"sizeArray size=${sizeArray.mkString(" ")}")
+
+  override def head = combine( leftValues(categoryIndex), rightStreams(categoryIndex).head )
+
+  override def hasNext =
+    rightStreams(categoryIndex).touched && rightStreams(categoryIndex).hasNext ||
+      categoryIndex < rightStreams.size - 1
+  
+  override def next = {
+    require(hasNext)
+    info(s"[next:] categoryIndex=$categoryIndex/${rightStreams.size}, touched=${rightStreams(categoryIndex).touched}")
+    if (rightStreams(categoryIndex).touched) {
+      // check if going to next element switches to next inner enum
+      if (!rightStreams(categoryIndex).hasNext) {
+        categoryIndex += 1
+      }
+    }
+    else {
+      categoryIndex += 1
+    }
+    info(s"invoking super.next with $ind")
+//    super.next
+    rightStreams(categoryIndex).touched = false
+    info(s"leftValues(categoryIndex)=${leftValues(categoryIndex).toString}")
     combine( leftValues(categoryIndex), rightStreams(categoryIndex).next )   
   }
 
