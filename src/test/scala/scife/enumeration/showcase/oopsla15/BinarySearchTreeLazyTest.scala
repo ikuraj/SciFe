33c1cd45be9731b6f1933723782aa30b399f48a7
Ivan Kuraj <ivan.kuraj@gmail.com>
2015-06-09 02:43:22 -0400

    Clean up lazy example

--- src/test/scala/scife/enumeration/showcase/oopsla15/BinarySearchTreeLazyTest.scala
+++ src/test/scala/scife/enumeration/showcase/oopsla15/BinarySearchTreeLazyTest.scala
@@ -17,99 +17,86 @@ import org.scalameter.api._
 
 import scala.language.postfixOps
 
+// import DSL
+import Enum._
+import Depend._
+import lazytraversal._
+
+// import the lazy data structure
+import common._
+import enumdef._
+import scife.util.structures._
+import LazyBSTrees._
+
 class BinarySearchTreeLazyTest extends FunSuite with Matchers with GeneratorDrivenPropertyChecks with HasLogger with ProfileLogger {
 
-  // import DSL
-  import Enum._
-  import Depend._
-  import lazytraversal._
-
-  import common._
-  import enumdef._
-  import scife.util.structures._
-  import LazyBSTrees._
-
-  type EnumType[A] = Finite[A] with Touchable[A] with Resetable[A] with Skippable[A]
-  type DepEnumType[I, O] = Depend[I, O] { type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A] }
-  type DepEnumTypeFinite[I, O] = DependFinite[I, O] { type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A] }
-
   test("Binary search tree, lazy enumeration") {
+    import BinarySearchTreeLazyTest._
 
     val bst =
       new WrapFunctionTest[(Int, Range), Tree, EnumType](
-        ((self: DepEnumType[(Int, Range), Tree], pair: (Int, Range)) => {
+        (self: DepEnumType[(Int, Range), Tree], pair: (Int, Range)) => {
           val (size, range) = pair
 
-          if (size < 0) throw new RuntimeException
-          if (size <= 0) new e.Singleton((Leaf: Tree)) with Touchable[Tree] with Resetable[Tree] with NoSkip[Tree] {
-            override def toString = s"Singleton[$hashCode]"
-          }
-          else if (range.isEmpty) Empty
-          else if (size == 1)
-            new e.WrapArray(range map { v => (Node(Leaf, v, Leaf): Tree) } toArray) with Touchable[Tree] with Resetable[Tree] with NoSkip[Tree] {
-
-              //            override def toString = s"Array[$hashCode](${toList})"
-              override def toString = s"Array[$hashCode]()"
-            }
+          if (size <= 0) new e.Singleton((Leaf: Tree)) with Touchable[Tree] with Resetable[Tree] with NoSkip[Tree]
           else {
-            val leftSizes = e.Enum(0 until size)
-            val roots = e.Enum(range)
-
-            val rootLeftSizePairs = e.Product(leftSizes, roots)
-
             val leftTrees: DepEnumTypeFinite[(Int, Int), Tree] = new InMap(self, { (par: (Int, Int)) =>
               val (leftSize, median) = par
               (leftSize, range.start to (median - 1))
             }) with DependFinite[(Int, Int), Tree] {
-              override type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A]
+              override type EnumSort[A] = BinarySearchTreeLazyTest.EnumType[A]
             }
 
-            val rightTrees: DepEnumTypeFinite[(Int, Int), Tree] =
-              new InMap(self, { (par: (Int, Int)) =>
-                val (leftSize, median) = par
-                (size - leftSize - 1, (median + 1) to range.end)
-              }) with DependFinite[(Int, Int), Tree] {
-                override type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A]
-              }
+            val rightTrees: DepEnumTypeFinite[(Int, Int), Tree] = new InMap(self, { (par: (Int, Int)) =>
+              val (leftSize, median) = par
+              (size - leftSize - 1, (median + 1) to range.end)
+            }) with DependFinite[(Int, Int), Tree] {
+              override type EnumSort[A] = BinarySearchTreeLazyTest.EnumType[A]
+            }
 
-            val leftRightPairs: DepEnumTypeFinite[(Int, Int), LazyTuple2[Tree, Tree]] =
-              lazytraversal.dependent.ProductFinite(leftTrees, rightTrees)
-
-            val fConstructTree: ((Int, Int), => LazyTuple2[Tree, Tree]) => Tree =
-              (p1, p2) => {
-                //              print(s"map invoked: ${p1}")
-                Node(p2._1, p1._2, p2._2)
-              }
-
-            val allNodes =
-              new lazytraversal.ChainFiniteSingleCombine[(Int, Int), LazyTuple2[Tree, Tree], Tree](
-                rootLeftSizePairs, leftRightPairs,
-                fConstructTree) with Touchable[Tree] {
-                override def toString = s"ChainFiniteSingleCombine[$hashCode]"
-              }
-
-            allNodes: BinarySearchTreeLazyTest.this.EnumType[Tree]
+            new lazytraversal.ChainFiniteSingleCombine(
+              (0 until size) ⊗ range,
+              lazytraversal.dependent.ProductFinite(leftTrees, rightTrees),
+              fConstructTree
+            ) with Touchable[Tree]
           }
-        }): (DepEnumType[(Int, Range), Tree], (Int, Range)) => EnumType[Tree])
+        })
 
-    val bound = 8
-
+    val size = 5
     var count = 0
-    for (x <- 1 to bound) {
-      val enum = bst(bound-1, 1 to bound)
+
+    // test for inserting all elements up to size
+    for (x <- 1 to size) {
+      val enum = bst(size - 1, 1 to size)
       var nextInd = 0
+
       while (nextInd < enum.size) {
-        enum.reset
+        // get the next tree
         val t = enum(nextInd)
         val newT = t insert x
-        assert( newT.lazyInvariant )
+        assert(newT.lazyInvariant)
 
+        // get next index potentially skipping elements
         nextInd = enum.next(nextInd)
         count += 1
+        // after each operation, reset enumerator tracking mechanism
+        enum.reset
       }
     }
-    
-    println(s"For trees of size $size, enumerated only $count out of total ${bst(bound-1, 1 to bound).size * bound} trees.")
+
+    println(s"For trees of size $size, enumerated only $count out of total ${bst(size - 1, 1 to size).size * size} trees.")
   }
 
+}
+
+object BinarySearchTreeLazyTest {
+  
+  type EnumType[A] = Finite[A] with Touchable[A] with Resetable[A] with Skippable[A]
+  type DepEnumType[I, O] = Depend[I, O] { type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A] }
+  type DepEnumTypeFinite[I, O] = DependFinite[I, O] { type EnumSort[A] = BinarySearchTreeLazyTest.this.EnumType[A] }
+  
+  // helper function that constructs trees
+  val fConstructTree: ((Int, Int), => LazyTuple2[Tree, Tree]) => Tree =
+    (p1, p2) => Node(p2._1, p1._2, p2._2)
+  
 }
